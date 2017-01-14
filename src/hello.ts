d19d06fe1a0863650c17fdbd89a96d934ee48d97
Robert F. Dickerson <rfdickerson@gmail.com>
2017-01-14 10:14:01 -0600

    Using computed properties

--- src/hello.ts
+++ src/hello.ts
@@ -1,13 +1,38 @@
 import * as ko from "knockout";
 
-class HelloViewModel {
-    language: KnockoutObservable<string>
-    framework: KnockoutObservable<string>
+class AppViewModel {
 
-    constructor(language: string, framework: string) {
-        this.language = ko.observable(language);
-        this.framework = ko.observable(framework);
+    firstName: KnockoutObservable<string>
+    lastName: KnockoutObservable<string>
+    fullName: KnockoutComputed<string>  
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = ko.observable("Bert");
+        this.lastName = ko.observable("Bertington");
+
+        this.fullName = ko.computed(function() {
+            return this.firstName() + " " + this.lastName();
+            }, this);        
     }
+
+    
 }
 
-ko.applyBindings(new HelloViewModel("Robert", "Dickerson"));
+ko.applyBindings(new AppViewModel("Bert", "Bertington"));
+
+// class HelloViewModel {
+//     language: KnockoutObservable<string>
+//     framework: KnockoutObservable<string>
+
+//     sum: KnockoutObservable<number>
+
+//     constructor(language: string, framework: string, sum: number) {
+//         this.language = ko.observable(language);
+//         this.framework = ko.observable(framework);
+
+//         this.sum = ko.observable(sum);
+
+//     }
+// }
+
+// ko.applyBindings(new HelloViewModel("Robert", "Dickerson", 0));
