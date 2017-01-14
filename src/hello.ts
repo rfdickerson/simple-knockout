bbae5be6862acc86568f751caa992e788eca8ae0
Robert F. Dickerson <rfdickerson@gmail.com>
2017-01-14 10:23:47 -0600

    Capitalize button

--- src/hello.ts
+++ src/hello.ts
@@ -15,6 +15,11 @@ class AppViewModel {
             }, this);        
     }
 
+    capitalizeLastName () {
+        var currentVal = this.lastName();
+        this.lastName(currentVal.toUpperCase());
+    }
+
     
 }
 
